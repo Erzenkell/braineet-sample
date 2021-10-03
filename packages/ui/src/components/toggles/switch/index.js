32e79bab5f43c0e192e8866bdc982dd45af6f214
Erzenkel <ferrier.sammy@gmail.com>
2021-10-03 13:17:13 +0200

    Ajout checkbox

--- packages/ui/src/components/toggles/switch/index.js
+++ packages/ui/src/components/toggles/switch/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { CheckBoxWrap, CheckBoxLabel, CheckBox } from './styles';
 
-/**
- * The `Toggle` component.
- */
+export const Switch = Prop => {
+    const { checked } = Prop;
+    const { label } = Prop;
 
-export const Switch = () => {
-    return <>Write your component</>;
+    return (
+        <div>
+            <CheckBoxWrap>
+                {checked === true ? (<CheckBox id="checkbox" type="checkbox" disabled />) : (<CheckBox id="checkbox" type="checkbox" />)}
+                {/* <LabelText> */}
+                {label}
+                {/* </LabelText> */}
+                <CheckBoxLabel htmlFor="checkbox" />
+            </CheckBoxWrap>
+        </div>
+    );
 };
 
-Switch.propTypes = {};
-Switch.defaultProps = {};
+Switch.propTypes = {
+    checked: PropTypes.bool,
+    label: "checked",
+};
+
+Switch.defaultProps = {
+    label: 'This is a label',
+};
+
 export default Switch;
--- packages/ui/src/components/toggles/switch/styles.js
+++ packages/ui/src/components/toggles/switch/styles.js
@@ -1,3 +1,48 @@
-/* stylelint-disable no-descending-specificity */
+import styled from 'styled-components';
 
-// import styled from 'styled-components';
+export const CheckBoxWrap = styled.div`
+    position: relative;
+`;
+export const CheckBoxLabel = styled.label`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 45px;
+    height: 25px;
+    border-radius: 15px;
+    background: #000000;
+    cursor: pointer;
+    &::after {
+        content: '';
+        display: block;
+        border-radius: 50%;
+        width: 19px;
+        height: 20px;
+        margin: 3px;
+        background: #ffffff;
+        transition: 0.2s;
+    }
+`;
+
+export const CheckBox = styled.input`
+    opacity: 0;
+    width: 50px;
+    height: 15px;
+    &:checked + ${CheckBoxLabel} {
+        background: #44c452;
+        &::after {
+            content: '';
+            display: block;
+            border-radius: 50%;
+            width: 19px;
+            height: 20px;
+            margin-left: 21px;
+            transition: 0.2s;
+        }
+    }
+`;
+
+// export const LabelText = styled.text`
+//     color: #44C452;
+//     font: 1em, poppins;
+// `;
